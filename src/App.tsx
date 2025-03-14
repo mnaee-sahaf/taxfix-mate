eeae676efeaf0247f933de2e1551df8f7f2146b2
Muneeb Naeem <muneeb.naeem95@gmail.com>
2025-03-14 22:49:51 +0500

    updating tax filing flow

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import CalculatorPage from "./pages/Calculator";
 import TaxFiling from "./pages/TaxFiling";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
-import TaxManagement from "./pages/TaxManagement";
+import { TaxData } from "./components/tax-filing/types";
 
 const queryClient = new QueryClient();
 
@@ -24,7 +24,9 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/calculator" element={<CalculatorPage />} />
-          <Route path="/filing" element={<TaxManagement />} />
+          <Route path="/filing" element={<TaxFiling updateTaxData={function (data: TaxData): void {
+            throw new Error("Function not implemented.");
+          } }/>} />
           <Route path="/profile" element={<Profile />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
