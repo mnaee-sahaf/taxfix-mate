5ebbee2cb6aeea6f87d9af074f9f736f4f1cbac6
Muneeb Naeem <muneeb.naeem95@gmail.com>
2025-04-20 16:58:49 +0500

    updating CTA section, removed button for calc

--- src/components/home/CTASection.tsx
+++ src/components/home/CTASection.tsx
@@ -32,12 +32,6 @@ const CTASection = () => {
                 <ArrowRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
               </Button>
             </Link>
-            
-            <Link to="/calculator">
-              <Button size="lg" variant="outline" className="rounded-full">
-                Try Tax Calculator
-              </Button>
-            </Link>
           </div>
           
           <div className="mt-8 text-sm text-muted-foreground">
